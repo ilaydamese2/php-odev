26be01fab6f246f2927dc66c7b3752e09a30ad04
ilaydamese2 <ilaydamese76@gmail.com>
2023-09-19 21:21:55 +0300

    session

---
+++ phpörnek7/index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+      echo "ILAYDA Adıyla Tanımlı Session Değeriniz : " . $_SESSION["ILAYDA"] . "<br />";
+     
+
+
+
+?>
+
+</body>
+</html> 
