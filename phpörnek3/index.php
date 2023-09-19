43f9ccbb5610f609c94dcec5d45ab800e60ce279
ilaydamese2 <ilaydamese76@gmail.com>
2023-09-19 19:49:35 +0300

    post yöntemi

---
+++ phpörnek3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="sonuc.php" method="post">
+        Isim : <input type="text" name="ad"><br />
+        Soyisim : <input type="text" name="soyad"><br />
+        <input type="submit" value="Gönder">
+</form>
+
+</body>
+</html>
---
+++ phpörnek3/sonuc.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+    $Isim   =   $_POST["ad"];
+    $Soyisim  =  $_POST["soyad"];
+
+    echo $Isim . " " . $Soyisim;
+    
+    ?>
+</body>
+</html>
